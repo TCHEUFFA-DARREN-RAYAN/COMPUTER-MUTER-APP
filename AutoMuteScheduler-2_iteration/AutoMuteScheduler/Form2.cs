93505510e4f392374397d07f27024470b0cc2368
TCHEUFFA <tcheuffadarren1@gmail.com>
2025-04-11 22:49:09 -0300

    here i made sure every user selection opens in a different window and the effect takes place on all the pages

---
+++ AutoMuteScheduler-2_iteration/AutoMuteScheduler/Form2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+public partial class Form2 : Form
+{
+    public DateTime SelectedDate { get; set; } // Property to store the selected date
+
+    public Form2()
+    {
+        InitializeComponent();
+    }
+
+    // Event handler for the 'Set Date' button
+    private void btnSetDate_Click(object sender, EventArgs e)
+    {
+        // Set the selected date and close the form
+        SelectedDate = dateTimePicker.Value;
+        DialogResult = DialogResult.OK; // Indicate success
+        Close();
+    }
+}
